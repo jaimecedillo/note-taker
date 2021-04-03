3c8f2ba2883112be4cde9e47f9ecf43274d51136
jaime <jaime@MacBook-Pro.local>
2021-04-03 15:56:15 -0600

    get api notes

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const { notes } = require('./db/db.json');
 
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
-app.use(express.json());
+// app.use(express.json());
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+    console.log(notes);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
