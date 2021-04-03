1291beec5891d6b6d6decc8b1a71bc3e971d49b8
jaime <jaime@MacBook-Pro.local>
2021-04-03 16:37:57 -0600

    post notes working

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const app = express();
 const { notes } = require('./db/db.json');
 
 // parse incoming string or array data
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     console.log(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    notes.push(req.body)
+    res.json(req.body);
+});
 
 
 app.listen(PORT, () => {
